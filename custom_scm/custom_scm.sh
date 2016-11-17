afb25b2eba590e466c4e9d4d59c13e3a5c2e96fa
matthewalbani <Matthew.jalbani@gmail.com>
2016-11-16 22:47:43 -0800

    move notifications into the scm script

--- custom_scm/custom_scm.sh
+++ custom_scm/custom_scm.sh
@@ -1,3 +1,16 @@
 set -v
 wget https://raw.githubusercontent.com/solanolabs/open_source_files/master/scipy/solano.yml -O solano.yml
-sed -i -e "s/\$ZULIP_STREAM/$ZULIP_STREAM/g" solano.yml
+cat >>solano.yml <<EOL
+notify:
+- channel: email
+  trigger-on:
+    - failed
+    - error
+    - setupfailed
+    - unconfigured
+    - checkoutfailed
+  branch: master
+  subject: "%repo%"
+  body: "Build failed with %error-count% error(s) and %passed-count% success(es) https://ci.solanolabs.com:443/reports/%build-id%"
+  recipients: $ZULIP_STREAM
+EOL
